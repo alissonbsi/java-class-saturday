3da4a1e175b8804d490bea16f7248c0232054d2d
bsi.alisson <bsi.alisson@gmail.com>
2025-11-29 11:11:16 -0300

    Aula sobre arrays

--- src/br/com/estudo/LeituraDados.java
+++ src/br/com/estudo/LeituraDados.java
@@ -20,6 +20,7 @@ public class LeituraDados {
         Scanner leitor = new Scanner(System.in); //nova instância de Scanner
 
 //        ESCREVA("Digite o seu nome: ")
+        // LEIA(variavel)
         System.out.println("Digite o seu nome: ");
         String meuNome = leitor.nextLine();
 
