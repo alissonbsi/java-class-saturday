72c60e0ba443ac7d3093dc3c66a30487cd2f5424
jeanderson11 <jeandersondejesus8@gmail.com>
2025-12-04 10:01:50 -0300

    implementando boas práticas

--- src/br/com/estudo/jeanderson/LoginUsuario.java
+++ src/br/com/estudo/jeanderson/LoginUsuario.java
@@ -3,39 +3,40 @@ package br.com.estudo.jeanderson;
 import java.util.Scanner;
 
 public class LoginUsuario {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int senha = 0;
-        String nome = "";
-        nome=username(nome);
-        senha=password(senha);
-        System.out.println("username: ".concat(nome));
-        System.out.println("Password: ".concat(String.valueOf(senha)));
+        String username = username();
+        int password = password();
+        System.out.println("username: ".concat(username));
+        System.out.println("Password: ".concat(String.valueOf(password)));
+        System.out.println("parabens, ".concat(username).concat("! Você realizou seu cadastro com sucesso!"));
     }
-    private static String username(String nome){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("digite seu nome: ");
-        nome= sc.nextLine();
-        return nome;
+
+    private static String username(){
+        System.out.println("Digite seu nome: ");
+
+        return sc.nextLine();
     }
-    private static int password(int senha){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("digite sua senha");
-        senha= sc.nextInt();
-        senha=validarPassword(senha);
-        return senha;
+
+    private static int password(){
+        System.out.println("Digite sua senha");
+
+        return validarPassword(sc.nextInt());
     }
-    private static int validarPassword(int senha){
-        Scanner sc= new Scanner(System.in);
-        String valorSenha= String.valueOf(senha);
-        if (valorSenha.length()==4) {
+
+    private static int validarPassword(Integer senha){
+        if (senha.toString().length() == 4) {
             System.out.println("senha valida\n");
+
             return senha;
         }
         else {
             System.out.println("senha invalida, digite novamente!");
             System.out.println("sua senha deve conter apenas 4 numeros inteiros!");
-            senha = sc.nextInt();
-            return validarPassword(senha);
+
+            return validarPassword(sc.nextInt());
         }
     }
 }
